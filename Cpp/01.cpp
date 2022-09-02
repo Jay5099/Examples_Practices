926a2fd77890abbc532eabd324e5843db070767b
Jay <jaypatel4099@gmail.com>
2022-09-02 22:03:18 +0530

    changedd_first time

---
+++ Cpp/01.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+    int main(){
+        float a;
+        std::cout<<"write a num:";
+        std::cin>>a;
+        std::cout<<"The number after devided by 10 is:"<<a/10<<std::endl;
+        std::cout<<"oh! "<<a<<" is my favourite number!!";
+        return 0;
+    } 
---
+++ Cpp_win/num_dis.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+    int main(){
+        float a;
+        std::cout<<"write a num:";
+        std::cin>>a;
+        std::cout<<"hi:"<<a;
+        return 0;
+    }
