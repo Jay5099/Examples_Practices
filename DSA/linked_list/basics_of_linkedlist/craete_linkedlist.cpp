b6f52159c2a02ef089588a800160fbb84fc54eea
Jay <jaypatel4099@gmail.com>
2022-11-08 13:47:05 +0530

    added new foldwer basic linkedkist

---
+++ DSA/linked_list/basics_of_linkedlist/craete_linkedlist.cpp
@@ -0,0 +1,51 @@
+/* create linked list by user only  */
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*next;
+};
+
+void printlist(node*p){
+    while(p!=NULL){
+            cout<<p->data<<"->";
+            p=p->next;
+        }
+        cout<<"\n";
+}
+
+int main(){
+
+    int n,i{2};
+    node*first=NULL;
+    node*last=NULL;
+    first =new node();
+
+    cout<<"Enter size of linked list:";
+    cin>>n;
+
+    cout<<"Enter elments 1st:";
+    cin>>first->data;
+    first->next=NULL;
+    last=first;
+    printlist(first);
+
+    while(n-1){
+        
+        node*temp=NULL;
+        temp=new node();
+        cout<<"Enter elments "<<i++<<"st:";
+
+        cin>>temp->data;
+        temp->next=NULL;
+        last->next=temp;
+        last=temp;
+
+        cout<<"This is link list:"<<endl;
+        printlist(first);
+        n--;
+    }
+}
