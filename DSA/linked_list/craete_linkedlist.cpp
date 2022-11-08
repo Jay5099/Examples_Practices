308d504f1a35b9c8807e3c23311540596b99f2ab
Jay <jaypatel4099@gmail.com>
2022-11-08 11:37:16 +0530

    linked list ds update in win only-01

---
+++ DSA/linked_list/craete_linkedlist.cpp
@@ -0,0 +1,26 @@
+/* create linked list by user only  */
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node*next;
+};
+
+int main(){
+
+    int n;
+    node*first=NULL;
+    first =new node();
+
+    cout<<"Enter size of linked list:";
+    cin>>n;
+    while(n){
+
+        node*temp=NULL;
+        temp=new node();
+        n--;
+    }
+}
