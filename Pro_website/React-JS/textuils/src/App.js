bca359eaefee2ba405a5bf2fe56d889baed6bb01
Jay <jaypatel4099@gmail.com>
2022-11-28 12:06:10 +0530

    textutils made

--- Pro_website/React-JS/textuils/src/App.js
+++ Pro_website/React-JS/textuils/src/App.js
@@ -2,26 +2,53 @@
 
 // import logo from "./logo.svg";
 import "./App.css";
-let a = " patel jay";
+// let a = " patel jay";
 function App() {
   return (
     <>
-      <nav>
-        <li>home</li>
-        <li>about</li>
-        <li>contact</li>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Textutils
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
       </nav>
-      <div className="container">
-        <h1>
-          Hello <b>{a}</b>
-        </h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. At labore
-          quia consequuntur. Ad molestiae eveniet provident facilis laudantium
-          maiores vel labore reprehenderit omnis quasi quibusdam fugiat eaque
-          distinctio, quidem sunt!
-        </p>
-      </div>
     </>
   );
 }
