f8063e7f421c150c2fb26ea74e575f287cd47a1c
Jay <jaypatel4099@gmail.com>
2022-11-29 11:31:46 +0530

    dark mode remodify

--- Pro_website/React-JS/textuils/src/Componenets/Navbar.js
+++ Pro_website/React-JS/textuils/src/Componenets/Navbar.js
@@ -1,83 +1,39 @@
 /** @format */
 
-import React,{useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
 
-  const [myStyle, setMyStyle] = useState({
-    color:'black',
-    backgroundColor:'white'
-  })
-
-  const [btntext, setbtntext] = useState("Enable Dark Mode")
-
-  const toggleStyle=()=>{
-    if(myStyle.color==='white'){
-      setMyStyle({
-        color:'black',
-        backgroundColor:'white'
-      })
-      setbtntext("Enable Dark Mode")
-    }
-    else{
-      setMyStyle({
-        color:'white',
-      backgroundColor:'black',
-      // border:'1px solid white'
-      })
-      
-      setbtntext("Enable Light Mode")
-    }
-  }
 
   return (
-    <nav className='navbar navbar-expand-lg bg-light'>
-      <div className='container-fluid'style={myStyle}>
-        <a className='navbar-brand' href='/'style={myStyle}>
-          {props.title}
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='/'style={myStyle}>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='/'style={myStyle}>
-                {props.aboutText}
-              </a>
-            </li>
-            <li className='nav-item'>
-              <button type="button" onClick={toggleStyle} className="btn btn-primary">{btntext}</button>
-            </li>
-          </ul>
-          <form className='d-flex' role='search'>
-            <input
-              className='form-control me-2'
-              type='search'
-              placeholder='Search'
-              aria-label='Search'
-            />
-            <button className='btn btn-primary' type='submit'>
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-    </nav>
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="navbar-collapse" id="navbarSupportedContent" >
+      <ul className="navbar-nav mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+     
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">{props.aboutText}</a>
+        </li>
+     
+        
+      </ul>
+      
+    <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`} style={{backgroundColor:'#white',borderRadius:'18px',margin:'1px 0px',padding:'5px',float:'right',alignContent:'flex'}}>
+      <input className="form-check-input"onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+      <label className="form-check-label" for="flexSwitchCheckDefault">Dark Mode</label>
+    </div>
+    </div>
+
+  </div>
+</nav>
   )
 }
 Navbar.propTypes = {
--- Pro_website/React-JS/textuils/src/Componenets/TextForm.js
+++ Pro_website/React-JS/textuils/src/Componenets/TextForm.js
@@ -28,6 +28,15 @@ export default function TextForm(props) {
     let newText=reverseString(text); 
       setText(newText)
   }
+  const handleCopyTextclick=()=>{
+    var text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleExtraSpace=()=>{
+    let newText=text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
   
   const handleOnChange = (event) => {
     // console.log('onchange');
@@ -39,10 +48,11 @@ export default function TextForm(props) {
   //setText("new text"); //correct way
   return (
     <>
-    <div className="container">
+    <div className="container"style={{color:props.mode==='dark'?'white':'black'}}>
       <h1>{props.heading}</h1>
       <div className='mb-3'>
-        <textarea className='form-label' value={text} onChange={handleOnChange} id='myBox' cols={150} rows={12} ></textarea>
+        <textarea className='form-label' value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#101010':'white',
+        color:props.mode==='dark'?'white':'black'}}id='myBox' cols={150} rows={12} ></textarea>
       </div>
       <button className='btn btn-primary mx-1' onClick={handleUpClick}>
         Convert to UpperCase
@@ -50,15 +60,17 @@ export default function TextForm(props) {
       <button className='btn btn-primary mx-1' onClick={handleLoClick}>Convert to LowerCase</button>
       <button className='btn btn-primary mx-1' onClick={handlelearClick}>Clear Text</button>
       <button className='btn btn-primary mx-1' onClick={handlereverseclick}>Reverse Text</button>
+      <button className='btn btn-primary mx-1' onClick={handleCopyTextclick}>Copy Text</button>
+      <button className='btn btn-primary mx-1' onClick={handleExtraSpace}>Remove Extra Space</button>
     </div>
-    <div className="container">
+    <div className="container"style={{color:props.mode==='dark'?'white':'black'}}>
       <h1>Your Text Summary </h1>
       <p>{text.split(" ").length} words and {text.length} characters</p>
       <p>{0.008* text.split(" ").length} Minutes read</p>
       <h3>Preview</h3>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter somthing First"}</p>
       </div>
-    </>
+    </> 
   )
 
   }
