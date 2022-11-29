176ca2be15aceb6667fbd683464eec1f995e4b8f
Jay <jaypatel4099@gmail.com>
2022-11-29 22:44:58 +0530

    on new repo git

--- Pro_website/React-JS/textuils/src/App.js
+++ Pro_website/React-JS/textuils/src/App.js
@@ -2,17 +2,17 @@
 
 // import logo from "./logo.svg";
 import './App.css'
-import About from './Componenets/About'
+// import About from './Componenets/About' 
 import Navbar from './Componenets/Navbar'
 import TextForm from  './Componenets/TextForm'
 import React,{useState} from 'react'
 import Alert from './Componenets/Alert'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";  
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";  
 
 function App() {
 
@@ -44,20 +44,20 @@ function App() {
   }
   return (
     <>
-    <Router>
+    {/* <Router> */}
       <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode}/>
       <Alert alert={alert}/>
       <div className='container my-3'>
-      <Switch>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/">
+      {/* <Switch> */}
+          {/* <Route exact path="/about"> */}
+            {/* <About /> */}
+          {/* </Route> */}
+          {/* <Route exact path="/"> */}
             <TextForm showAlert={showAlert} heading='Enter the text to analyze'mode={mode}/>
-          </Route>
-        </Switch>
+          {/* </Route> */}
+        {/* </Switch> */}
       </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
